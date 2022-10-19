4493b184d0e7f94d1276cb6b47708f2e4ab516ee
Bogdan <upwork@landingpage.com>
2022-10-19 18:20:06 +0200

    annoncemend bar and desktop header ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
 			colors: {
-				accent: 'red',
-				text: '#dadada'
+				accent: '#008060',
+				textColor: '#1d1d1d'
 			},
 			container: {
 				center: true,
@@ -17,7 +17,21 @@ module.exports = {
 				'lg': { max: "1024px" },
 				'md': { max: "768px" },
 				'sm': { max: "640px" }
-			}
+			},
+			animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
 		},
   },
   plugins: [],
